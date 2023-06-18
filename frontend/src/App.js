ee5ecea57bf521f7ebd68b4e1d3d9b46e54d2e9d
Divyanshu Yadav <divyanshuyadavds27@gmail.com>
2023-06-18 18:06:03 +0530

    frontend required file created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route, BrowserRouter as Router, Link, Routes} from 'react-router-dom';
 
 function App() {
 
   return (
     <div className="App">
 
+
+      <Router>
+          {/* <Header /> */}
+          
+      </Router>
+
+
+
       <header className="App-header">
-        <img src={logo} className="App-logo" />
-        
+        <img src={logo} className="App-logo" />  
       </header>
+  
+
     </div>
   );
 }
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+    return (<h1>Jai Shree Ram</h1>);
+};
+
+export default Home;
