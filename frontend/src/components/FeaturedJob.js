ea628b10f05d33089e7db4754564a3437566b305
Divyanshu Yadav <divyanshuyadavds27@gmail.com>
2023-06-27 16:35:44 +0530

    modified card section!!

--- frontend/src/components/FeaturedJob.js
+++ frontend/src/components/FeaturedJob.js
@@ -1,52 +1,108 @@
-import React from 'react'
-import '../style/featured_job.css';
-import icon from '../style/icon-1.png'
+import React from "react";
+import "../style/featured_job.css";
+import icon from "../style/icon-1.png";
 import { Link, useMatch, useResolvedPath } from "react-router-dom";
 
 const FeaturedJob = () => {
   return (
     <div className="container_f">
-        <h3 className="heading0">Featured<span style={{ color: '#26A4FF' }}> jobs</span></h3>
-        <div className="box-container_f">
-            <div className="box_f">
-                <img src={icon} alt="icon" />
-                <div className="btn_f">Full Time</div><br/>
-                <div className="job" color="#25324B">
-                <Link to="#" >Email Marketing</Link>
-                </div>
-                <div className="p">html(company) . Spain(address)</div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, commodi?</p>
-                <div className="btn_f0">Design</div>
-                <div className="btn_f0">Marketing</div>
+      <h3 className="heading0">
+        Featured<span style={{ color: "#26A4FF" }}> jobs</span>
+      </h3>
+      <div className="box-container_f">
+
+        <div className="box_f">
+
+          <div>
+            <img src={icon} alt="icon" />
+            <div className="btn_f">Full Time</div>
+            <br />
+          </div>
+
+          <div>
+            <div className="job">
+              <Link to="#">Email Marketing</Link>
+            </div>
+            <div className="p">html(company) . Spain(address)</div>
+          </div>
+
+          <div className="para">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
+                commodi?
+            </p>
             </div>
 
-            <div className="box_f">
-                <img src={icon} alt="icon" />
-                <div className="btn_f">Full Time</div><br/>
-                <div className="job">
-                <Link to="#" >Email Marketing</Link>
-                </div>
-                <div className="p">html(company) . Spain(address)</div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, commodi?</p>
-                <div className="btn_f0">Design</div>
-                <div className="btn_f0">Marketing</div>
-            </div>
-
-            <div className="box_f">
-                <img src={icon} alt="icon" />
-                <div className="btn_f">Full Time</div><br />
-                <div className="job">
-                <Link to="#" >Email Marketing</Link>
-                </div>
-                <div className="p">html(company) . Spain(address)</div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, commodi?</p>
-                <div className="btn_f0">Design</div>
-                <div className="btn_f0">Marketing</div>
-            </div>
+          <div>
+            <div className="btn_f0">Design</div>
+            <div className="btn_f0 btn_f01">Marketing</div>
+          </div>
 
         </div>
-    </div>  
-  )
-}
 
-export default FeaturedJob
+
+        <div className="box_f">
+
+          <div>
+            <img src={icon} alt="icon" />
+            <div className="btn_f ">Full Time</div>
+            <br />
+          </div>
+
+          <div>
+            <div className="job">
+              <Link to="#">Email Marketing</Link>
+            </div>
+            <div className="p">html(company) . Spain(address)</div>
+          </div>
+
+          <div className="para">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
+                commodi?
+            </p>
+            </div>
+
+          <div>
+            <div className="btn_f0">Design</div>
+            <div className="btn_f0 btn_f01">Marketing</div>
+          </div>
+
+        </div>
+
+
+        <div className="box_f">
+
+          <div>
+            <img src={icon} alt="icon" />
+            <div className="btn_f">Full Time</div>
+            <br />
+          </div>
+
+          <div>
+            <div className="job">
+              <Link to="#">Email Marketing</Link>
+            </div>
+            <div className="p">html(company) . Spain(address)</div>
+          </div>
+
+          <div className="para">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,
+                commodi?
+            </p>
+            </div>
+
+          <div>
+            <div className="btn_f0">Design</div>
+            <div className="btn_f0 btn_f01">Marketing</div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedJob;
