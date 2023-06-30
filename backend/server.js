b28b2059688e6e0fbe787a5439014479a4ae8807
sauravraj276 <sauravraj276@gmail.com>
2023-06-30 17:25:53 +0530

    api test

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/api', (req, res) => {
+  res.send('Hello from server')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
