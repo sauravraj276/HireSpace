2b5b470edeb44041424cd6f809afd01d49bc1af9
sauravraj276 <sauravraj276@gmail.com>
2023-06-30 22:27:40 +0530

    cluster created and connection made

---
+++ backend/config/db.js
@@ -0,0 +1,22 @@
+//mongodb connection
+
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+//asynchronous arrow function
+
+const connectDB = async() =>{
+    try{
+        await mongoose.connect(db,{
+            useNewUrlParser:true,
+            //useCreateindex:true
+        });
+        console.log("MongoDB connected....");
+    }catch(err){
+        console.error(err.message);
+        //exit process failure
+        process.exit(1);
+    }
+}
+module.exports=connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 const express = require('express')
-const app = express()
+const connectDB = require("./config/db");
 const port = 3000
+
+const app = express()
+connectDB();
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.get('/api', (req, res) => {
-  res.send('Hello from server')
-})
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
