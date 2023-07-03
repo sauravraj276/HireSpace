756be31d2d05409cfdad55785b2faff3ce8bb389
Divyanshu Yadav <divyanshuyadavds27@gmail.com>
2023-07-04 01:44:06 +0530

    candidate profile frontend done!

--- frontend/src/components/User/Profile_candidate.js
+++ frontend/src/components/User/Profile_candidate.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../../style/profile_candidate.css'
+import img from '../../images/divy.png'
 function Profile_candidate() {
   return (
     <>
@@ -7,70 +8,94 @@ function Profile_candidate() {
             <h1> Your Profile</h1>
             <div className="container-candidate">
                 <div className="left-profile">
-                    <img src='#' alt='profile-pic'></img>
-                    <div className="input-profile">
-                        <h1>Name</h1>
-                            <p>Divyanshu</p>
-                        <h1>Student/Company</h1>
-                    </div>
-                    <div className="input-profile">
-                        <h1>Summary</h1>
-                        <p className='desc-profile'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit unde eius fugiat consequatur accusantium, magnam consequuntur praesentium. Nobis quaerat neque voluptatibus natus accusamus temporibus nihil dolorem fuga optio omnis. Similique.</p>
-                    </div>
-                    <div className="input-profile">
-                        <p className='desc-profile'>Skills</p>
-                    </div>
+
+                    <img src={img} alt='profile-pic'></img>
+        
+                    <label>Name</label>
+                        <p className="input-profile">Divyanshu</p>
+                    <label>Student/Company</label>
+                
+                    <label>Summary</label>
+                    <p className='input-profile'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit unde eius fugiat consequatur accusantium, magnam consequuntur praesentium. Nobis quaerat neque voluptatibus natus accusamus temporibus nihil dolorem fuga optio omnis. Similique.</p>
+                
+                    <label>Skills</label>
+                    <p className='input-profile'>Skills</p>
                     
                 </div>
 
                 <div className="right-profile">
                     <div className="basic-info-profile">
-                        <div className="input-profile">
-                            <h1>Name</h1> 
-                            <p>Divyanshu</p>
-                        </div>
-                        <div className="input-profile">
-                            <h1>Experience</h1> 
-                            <p>3 Year</p>
-                        </div>
-                        <div className="input-profile">                            
-                            <h1>Phone no.</h1> 
-                            <p>+91 9192631770</p>
-                        </div>
-                        <div className="input-profile">                            
-                            <h1>CTC </h1>
-                            <p>7LPA</p>
-                        </div>
-                        <div className="input-profile">                            
-                            <h1>Location</h1> 
-                            <p>Noida</p>
-                        </div>
-                        <div className="input-profile">                            
-                            <h1>email </h1>
-                            <p>fakeprofile@gmail.com</p>
-                        </div>
-                        <div className="input-profile">                            
-                            <h1>download-resume</h1>
-                            <p>download</p>
-                        </div>
+                         <div className="align-box">
+                            <label>Name</label> 
+                            <p className="right-input-profile">Divyanshu</p>
+                         </div>
+
+                         <div className="align-box">
+                            <label>Experience</label> 
+                            <p className="right-input-profile">3 Year</p>
+                          </div> 
+                           <div className="align-box">                         
+                            <label>Phone no.</label> 
+                            <p className="right-input-profile">+91 9192631770</p>
+                            </div>
+                            <div className="align-box">                         
+                            <label>CTC </label>
+                            <p className="right-input-profile">7LPA</p>
+                            </div>
+                            <div className="align-box">                         
+                            <label>Location</label> 
+                            <p className="right-input-profile">Noida</p>
+                            </div>
+                             <div className="align-box">                    
+                            <label>email </label>
+                            <p className="right-input-profile">fakeprofile@gmail.com</p>
+                            </div>
+
+                            <label>download-resume</label>
+                            {/* <p className="right-input-profile">download</p>
+                         */}
                     </div>
 
-                    <div className="education-profile">
-                        <div className="input-profile">                            
-                            <h1>Education</h1>
-                        </div>
+                    <div className="education-profile basic-info-profile">
+                                                    
+                            <span>Education :</span>
+                            <div className="align-box0">
+                             <p className="right-input-profile">Lorem ipsum dolor sit amet consectetur adipisicing elit. At, ipsum quam nisi porro voluptatibus doloribus quibusdam non repellat delectus molestiae dolorum maiores, atque dignissimos sapiente tenetur ullam. Modi, natus aliquid.</p>
+                             <p className="right-input-profile">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae dolore quas repudiandae repellat velit mollitia, autem, adipisci, unde beatae dicta debitis atque ea cupiditate quidem explicabo praesentium et ipsa maxime?</p>
+                            </div>
+                            <div className="align-box0">
+                            <p className="right-input-profile">fakeprofile@gmail.com</p>
+                            <p className="right-input-profile">fakeprofile@gmail.com</p>
+                            </div>
+                            
                     </div>
 
-                    <div className="project">
-                        <div className="input-profile">                            
-                            <h1>Project</h1>
-                        </div>
+                    <div className="education-profile basic-info-profile">
+                                                    
+                            <span>Projects :</span>
+                            <div className="align-box0">
+                             <p className="right-input-profile">Lorem ipsum dolor sit amet consectetur adipisicing elit. At, ipsum quam nisi porro voluptatibus doloribus quibusdam non repellat delectus molestiae dolorum maiores, atque dignissimos sapiente tenetur ullam. Modi, natus aliquid.</p>
+                             <p className="right-input-profile">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae dolore quas repudiandae repellat velit mollitia, autem, adipisci, unde beatae dicta debitis atque ea cupiditate quidem explicabo praesentium et ipsa maxime?</p>
+                            </div>
+                            <div className="align-box0">
+                            <p className="right-input-profile">fakeprofile@gmail.com</p>
+                            <p className="right-input-profile">fakeprofile@gmail.com</p>
+                            </div>
+                            
                     </div>
 
-                    <div className="certificate">
-                        <div className="input-profile">                                                     
-                        <h1>Certificate</h1>
-                        </div>
+                    <div className="certificate education-profile basic-info-profile">
+                                                    
+                            <span>Certificate:</span>
+                            <div className="align-box0">
+                             <p className="right-input-profile">Lorem ipsum dolor sit amet consectetur adipisicing elit. At, ipsum quam nisi porro voluptatibus doloribus quibusdam non repellat delectus molestiae dolorum maiores, atque dignissimos sapiente tenetur ullam. Modi, natus aliquid.</p>
+                             <p className="right-input-profile">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae dolore quas repudiandae repellat velit mollitia, autem, adipisci, unde beatae dicta debitis atque ea cupiditate quidem explicabo praesentium et ipsa maxime?</p>
+                            </div>
+                            <div className="align-box0">
+                            <p className="right-input-profile">fakeprofile@gmail.com</p>
+                            <p className="right-input-profile">fakeprofile@gmail.com</p>
+                            </div>
+                            
                     </div>
                     
                 </div>
